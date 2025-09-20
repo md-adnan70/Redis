b9de70fea698b07737ca963213483a8c6e3df08d
adnan rao <rao.adnan.098@gmail.com>
2025-09-20 12:46:41 +0530

    external api cache

---
+++ external-api-caching/main.py
@@ -0,0 +1,36 @@
+import redis
+import json
+import hashlib
+import httpx
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+redis_client = redis.Redis(host='localhost',port=6379,db=0)
+
+class PostRequest(BaseModel):
+    post_id: int
+
+def make_cache_key(post_id: int):
+    raw = f"external_api:post_{post_id}"
+    return hashlib.sha256(raw.encode()).hexdigest()
+
+@app.post('/get-post')
+async def get_post(data: PostRequest):
+    cache_key = make_cache_key(data.post_id)
+
+    cache_data = redis_client.get(cache_key)
+    if cache_data:
+        print("served from redis cache")
+        return json.loads(cache_data)
+    
+    print('Calling external API.....')
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://jsonplaceholder.typicode.com/posts/{data.post_id}")
+        if  response.status_code != 200:
+            return {'error':'Post not found!'}
+        
+    post_data = response.json()
+    redis_client.setex(cache_key, 600, json.dumps(post_data))
+    print('Fetched from API and stored in Cache')
+    return post_data
